b3ed4f46ed197b279b45debd84288d867c74f35d
Rob Brennan <rob@therobbrennan.com>
2019-10-17 16:22:01 -0700

    Specify settings for fallback PostgreSQL values

--- hello/lib/hello/repo.ex
+++ hello/lib/hello/repo.ex
@@ -1,13 +1,14 @@
 defmodule Hello.Repo do
   use Ecto.Repo, otp_app: :hello, adapter: Ecto.Adapters.Postgres
 
+  # Define fallback values that match hello/config/dev.exs in case we are developing without Docker
   def init(_, config) do
     config = config
-      |> Keyword.put(:username, System.get_env("PGUSER"))
-      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
-      |> Keyword.put(:database, System.get_env("PGDATABASE"))
-      |> Keyword.put(:hostname, System.get_env("PGHOST"))
-      |> Keyword.put(:port, System.get_env("PGPORT") |> String.to_integer)
+      |> Keyword.put(:username, System.get_env("PGUSER") || "postgres")
+      |> Keyword.put(:password, System.get_env("PGPASSWORD") || "postgres")
+      |> Keyword.put(:database, System.get_env("PGDATABASE") || "hello_dev")
+      |> Keyword.put(:hostname, System.get_env("PGHOST") || "localhost")
+      |> Keyword.put(:port, (System.get_env("PGPORT") || "5432") |> String.to_integer)
     {:ok, config}
   end
 end
